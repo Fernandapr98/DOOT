114be062c048f760e42f9f85a80f0f3119065574
Fernandapr98 <118834228+Fernandapr98@users.noreply.github.com>
2022-12-13 15:42:43 -0800

    Update PruebaDeBuild.js

--- PruebaDeBuild.js
+++ PruebaDeBuild.js
@@ -5,7 +5,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200
 res.setHeader(‘Content-Type’, ‘text/plain’)
-res.end(‘Hello Worlddd peopleee\n’)
+res.end(‘Hello Worlddd\n’)
 })
 
 server.listen(port, hostname, () => {
